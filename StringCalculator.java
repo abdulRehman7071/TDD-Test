fea6144dd6c66eadcb89b46172a86585952b0316
abdulRehman7071 <karzbaba47@gmail.com>
2024-08-28 02:40:51 +0530

    Multiple numbers

--- StringCalculator.java
+++ StringCalculator.java
@@ -2,6 +2,13 @@ public class StringCalculator {
     public static int add(String numbers) {
         if (numbers == null || numbers.isEmpty()) {
             return 0;
+        } else if (numbers.contains(",")) {
+            String[] numberArray = numbers.split(",");
+            int sum = 0;
+            for (String number : numberArray) {
+                sum += Integer.parseInt(number);
+            }
+            return sum;
         } else {
             return Integer.parseInt(numbers);
         }
@@ -9,7 +16,7 @@ public class StringCalculator {
 
     public static void main(String[] args) {
         StringCalculator calculator = new StringCalculator();
-        int result = calculator.add("1");
-        System.out.println(result); // prints 1
+        int result = calculator.add("1,2,3");
+        System.out.println(result); // prints 6
     }
 }
