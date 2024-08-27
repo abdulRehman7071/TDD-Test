282a713d3c5173327a78546f7a4ca36156a40770
abdulRehman7071 <karzbaba47@gmail.com>
2024-08-28 02:42:44 +0530

    New lines between numbers

--- StringCalculator.java
+++ StringCalculator.java
@@ -2,21 +2,20 @@ public class StringCalculator {
     public static int add(String numbers) {
         if (numbers == null || numbers.isEmpty()) {
             return 0;
-        } else if (numbers.contains(",")) {
+        } else {
+            numbers = numbers.replace("\n", ",");
             String[] numberArray = numbers.split(",");
             int sum = 0;
             for (String number : numberArray) {
                 sum += Integer.parseInt(number);
             }
             return sum;
-        } else {
-            return Integer.parseInt(numbers);
         }
     }
 
     public static void main(String[] args) {
         StringCalculator calculator = new StringCalculator();
-        int result = calculator.add("1,2,3");
+        int result = calculator.add("1\n2,3");
         System.out.println(result); // prints 6
     }
 }
